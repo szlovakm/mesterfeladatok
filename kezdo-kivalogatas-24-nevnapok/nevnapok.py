e41f35a7a996507357956568514badb46f8ab0ac
szlovakm <szlovakm@gmail.com>
2018-12-07 21:38:14 +0100

    felesleges tarolas eltavolitasa

--- kezdo-kivalogatas-24-nevnapok/nevnapok.py
+++ kezdo-kivalogatas-24-nevnapok/nevnapok.py
@@ -1,10 +1,8 @@
 N, nev = input().split()
 N = int(N)
-nevek = []
 jonev = []
 for i in range(N):
     aktnev = input()
-    nevek.append(aktnev)
     if aktnev == nev:
         jonev.append(str(i+1))
 print(len(jonev), " ".join(jonev))
